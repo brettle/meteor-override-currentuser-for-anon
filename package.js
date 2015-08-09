f61e1d7db3567515e790835e25b2147e2d7b8b2d
Dean Brettle <dean@brettle.com>
2015-08-09 16:11:43 -0700

    Update git URL and bump version.

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'brettle:override-currentuser-for-anon',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: "Overrides Blaze's currentUser helper so that it returns null for anon user.",
   // URL to the Git repository containing the source code for this package.
-  git: 'git@github.com:brettle/meteor-override-currentuser-for-anon.git',
+  git: 'https://github.com/brettle/meteor-override-currentuser-for-anon.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
